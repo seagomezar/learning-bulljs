d83614f1e1c47e16e6a5f4af6270ce4f6adeba0c
Sebastian Alonso Gomez Arias <seagomezar@gmail.com>
2020-04-08 09:11:52 -0500

    Adding debug and using component lifecicle

--- example-1-simple-queue-and-job/queue.js
+++ example-1-simple-queue-and-job/queue.js
@@ -11,7 +11,7 @@ const init = () => {
   const queue = new bull("simple-queue", redisConfig); // creating the queue
   queue.process(async (job, done) => {
     console.log(`Starting Job ${job.id}`);
-    await send(job.data.xNumbers, job.data.ySeconds);
+    await count(job.data.xNumbers, job.data.ySeconds);
     console.log(`Job ${job.id} completed`);
     done(null, "Job done ");
   });
@@ -20,12 +20,12 @@ const init = () => {
   queue.add({ xNumbers: 20, ySeconds: 5 }, { retry: 2, delay: 10000 }); // And other
 
   /* Compare the approach
-  send(10, 10);
-  send(20, 5);
+  count(10, 10);
+  count(20, 5);
   */
 };
 
-const send = (xNumbers, ySeconds) => {
+const count = (xNumbers, ySeconds) => {
   return new Promise((accept) => {
     let x = 0;
     const intervalID = setInterval(() => {
