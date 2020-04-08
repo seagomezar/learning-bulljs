81dc8cfe76d2490b2d56725c888b206d7bc6731f
Sebastian Alonso Gomez Arias <seagomezar@gmail.com>
2020-04-08 08:27:04 -0500

    First example

---
+++ example-1-simple-queue-and-job/queue.js
@@ -0,0 +1,41 @@
+const bull = require("bull");
+const redisConfig = {
+  redis: {
+    port: 6379,
+    host: "127.0.0.1",
+    // password: "", if you are using production redis store the password with .env
+  },
+};
+
+const init = () => {
+  const queue = new bull("simple-queue", redisConfig); // creating the queue
+  queue.process(async (job, done) => {
+    console.log(`Starting Job ${job.id}`);
+    await send(job.data.xNumbers, job.data.ySeconds);
+    console.log(`Job ${job.id} completed`);
+    done(null, "Job done ");
+  });
+
+  queue.add({ xNumbers: 10, ySeconds: 10 }, { retry: 10, delay: 20001 }); // Here you add the job
+  queue.add({ xNumbers: 20, ySeconds: 5 }, { retry: 2, delay: 10000 }); // And other
+
+  /* Compare the approach
+  send(10, 10);
+  send(20, 5);
+  */
+};
+
+const send = (xNumbers, ySeconds) => {
+  return new Promise((accept) => {
+    let x = 0;
+    const intervalID = setInterval(() => {
+      if (++x === xNumbers) {
+        clearInterval(intervalID);
+        accept("Count finished");
+      }
+      console.log(x);
+    }, (ySeconds * 1000) / xNumbers);
+  });
+};
+
+init();
